a6274048c3fd43782769526c86df60c02f6083b4
Shashank <shashanknarain24@gmail.com>
2024-03-06 18:27:21 +0530

    ble callbacks and multiscan

--- esp32_sketch/esp32_sketch.ino
+++ esp32_sketch/esp32_sketch.ino
@@ -5,14 +5,28 @@
 #include <Wire.h>
 #include <AHT20.h>
 #include <RTClib.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
 
 #define LED 2
+
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
 
 AHT20 aht20;
 RTC_DS3231 rtc;
 
 String entry;
 const char* filePath = "/sensor_data.csv";
+
+BLEServer* pServer = NULL;
+BLECharacteristic* pCharacteristic = NULL;
+BLEService* pService = NULL;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+
 
 void setup()
 {
@@ -21,6 +35,21 @@ void setup()
 
 void loop()
 {
+
+
+  // disconnecting
+  if (!deviceConnected && oldDeviceConnected) {
+      delay(500); // give the bluetooth stack the chance to get things ready
+      pServer->startAdvertising(); // restart advertising
+      pService->start();
+      Serial.println("start advertising");
+      oldDeviceConnected = deviceConnected;
+  }
+
+  if (deviceConnected && !oldDeviceConnected) {
+      // do stuff here on connecting
+      oldDeviceConnected = deviceConnected;
+  }
   delay(1000);
   digitalWrite(LED, HIGH);
   delay(1000);
@@ -36,6 +65,29 @@ void loop()
 
 
 /************************************************************************/
+
+class ServerCallbacks: public BLEServerCallbacks {
+  void onConnect(BLEServer* pServer)
+  {
+    deviceConnected = true;
+    BLEDevice::startAdvertising();
+  }
+
+  void onDisconnect(BLEServer* pServer)
+  {
+    deviceConnected = false;
+  }
+};
+
+class CharacteristicCallbacks: public BLECharacteristicCallbacks {
+  void OnWrite(BLECharacteristic *pCharacteristic)
+  {
+    std::string value = pCharacteristic->getValue();
+    for (int i = 0;i<value.length();i++)
+      Serial.print(value[i]);
+    Serial.println();
+  }
+};
 
 void init()
 {
@@ -65,4 +117,36 @@ void init()
     while (1);
   }
   rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+
+  BLEDevice::init("ESP32_Sensor_1");
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new ServerCallbacks);
+  pService = pServer->createService(SERVICE_UUID);
+  
+
+  pCharacteristic = pService->createCharacteristic(
+                                          CHARACTERISTIC_UUID,
+                                          BLECharacteristic::PROPERTY_READ |
+                                          BLECharacteristic::PROPERTY_WRITE | 
+                                          BLECharacteristic::PROPERTY_NOTIFY |
+                                          BLECharacteristic::PROPERTY_INDICATE
+                                       );
+
+  pCharacteristic->setCallbacks(new CharacteristicCallbacks);
+
+  pCharacteristic->setValue("Hello World says Neil");
+  pService->start();
+  // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
+  pAdvertising->setMinPreferred(0x12);
+  BLEDevice::startAdvertising();
+  Serial.println("Characteristic defined! Now you can read it in your phone!");
+}
+
+void transferCSVData(String filePath)
+{
+  Serial.println("Transferring: filepath");
 }
